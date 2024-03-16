3cf0504b8898cf2857d064b1795fed399b660d12
aditya-7562 <aditya.12114424@lpu.in>
2024-03-16 22:14:56 +0530

    completed class 15 assignment

---
+++ src/class15/D.java
@@ -0,0 +1,13 @@
+package class15;
+
+import class15.A;
+
+public class D {
+    public static void main(String[] args) {
+        A obj = new A();
+        // System.out.println(obj.x);
+        // System.out.println(obj.y);
+        System.out.println(obj.z);
+        // System.out.println(obj.w);
+    }
+}
