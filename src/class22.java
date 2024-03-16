05933eac162894a693766df2fbc7d181e1bf29a8
aditya-7562 <aditya.12114424@lpu.in>
2024-03-16 23:51:38 +0530

    completed class 22 assignment

---
+++ src/class22.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class class22 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        for(int i = 0; i< n; i++){
+            System.out.println("please enter " + (i+1) + " element: ");
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Array is: ");
+        for(int i=0; i<n; i++){
+            System.out.println(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
