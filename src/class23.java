110f87e614220cd56b4dff3333e91ba92c6eb909
aditya-7562 <aditya.12114424@lpu.in>
2024-03-17 00:10:55 +0530

    completed class 23 assignment

---
+++ src/class23.java
@@ -0,0 +1,33 @@
+public class class23 {
+
+    static boolean isPrimeRam(int n){
+        if(n <= 1) return false;
+        if(n==2) return true;
+        for(int i = 2; i <= n; i++){
+            if(n%i == 0) return false;
+        }
+        return true;
+    }
+
+    static boolean isPrimeShyam(int n){
+        if(n <= 1) return false;
+        if(n==2) return true;
+        for(int i = 2; i <= Math.sqrt(n); i++){
+            if(n%i == 0) return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int n = 99999999;
+        long start = System.currentTimeMillis();
+        System.out.println(isPrimeRam(n));
+        long end = System.currentTimeMillis();
+        System.out.println("Time by RAM's approach: " + (end-start));
+        
+        start = System.currentTimeMillis();
+        System.out.println(isPrimeShyam(n));
+        end = System.currentTimeMillis();
+        System.out.println("Time by SHYAM's approach: " + (end-start));
+
+    }
+}
