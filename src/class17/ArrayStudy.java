22b57a7095beff4f9ae43558e93d2fd3c8b1900a
aditya-7562 <aditya.12114424@lpu.in>
2024-03-16 22:55:13 +0530

    completed class 17 assignment

---
+++ src/class17/ArrayStudy.java
@@ -0,0 +1,36 @@
+package class17;
+public class ArrayStudy {
+
+    static void print(int arr[]){
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int size = 5;
+        int arr[] = new int[size];
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        System.out.println(arr[4]);
+
+        print(arr);
+
+        for(int i=0; i<size; i++){
+            arr[i] = i+1;
+        }
+
+        print(arr);
+
+        int[] copyArray = new int[2*size];
+        for(int i = 0; i < size; i++){
+            copyArray[i] = arr[i];
+        }
+        copyArray[5] = 6;
+        copyArray[6] = 7;
+
+        print(copyArray);
+    }
+}
