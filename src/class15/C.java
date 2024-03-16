7b1f5a1b54927ae716164e4337bb8bb2a33ec528
aditya-7562 <aditya.12114424@lpu.in>
2024-03-16 22:17:08 +0530

    minor syntaxical changes

--- src/class15/C.java
+++ src/class15/C.java
@@ -1,7 +1,17 @@
 package class15;
 
-public class C {
+import class15.A;
+
+public class C extends A{
     public static void main(String[] args) {
-        
+        A obj = new A();
+        C childObj = new C();
+
+        // System.out.println(obj.x);
+        System.out.println(childObj.x);
+        // System.out.println(obj.y);
+        // System.out.println(childObj.y);
+        // System.out.println(obj.z);
+        System.out.println(childObj.w);
     }
 }
