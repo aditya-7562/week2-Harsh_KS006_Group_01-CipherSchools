888bc1456e9f582a13eef9fadbb9648ef477c3b7
aditya-7562 <aditya.12114424@lpu.in>
2024-03-16 21:46:19 +0530

    added class 14 assignment

---
+++ src/class14.java
@@ -0,0 +1,41 @@
+
+interface M{
+    int x = 10;
+    void fun();
+}
+
+interface N{
+    int x = 20;
+    void fun();
+}
+
+class C implements M, N{
+    public void fun(){
+        System.out.println("Hello I'm function in C");
+        System.out.println(M.x);
+        System.out.println(N.x);
+        System.out.println(M.super.hashCode());
+        System.out.println(N.super.hashCode());
+    }
+}
+
+interface O extends M, N{
+    void fun();
+}
+
+class D implements O{
+    public void fun(){
+        System.out.println("Hello I'm function in D");
+    }
+}
+
+public class class14 {
+
+    public static void main(String[] args) {
+        C c = new C();
+        c.fun();
+
+        D d = new D();
+        d.fun();
+    }
+}
